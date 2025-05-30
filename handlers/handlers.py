0c4285cf0d6d27007f98f0dc9be5c7d00e65b0aa
mia <mxssedxnactxon@gmail.com>
2025-05-30 15:09:09 +0700

    separate game function in sevices.py and added imports from that file

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -3,43 +3,10 @@ from aiogram.types import Message
 from aiogram.filters import Command, CommandStart
 from lexicon.lexicon import LEXICON_RU
 from keyboard.keyboard import start_keyboard, help_keyboard, game_keyboard, stat_keyboard
-import random
+from services.services import comp_choice, rsp, stat
 
 
 router = Router()
-
-def comp_choice():
-    return random.choice(['камень', 'ножницы', 'бумага'])
-
-def rsp(choice, computer_choice):
-    if choice == 'камень' and computer_choice == 'ножницы':
-        stat['wins'] += 1
-        return f'вы выйграли'
-    elif choice == 'камень' and computer_choice == 'бумага':
-        stat['loses'] += 1
-        return f'я выйграл'
-    elif choice == 'ножницы' and computer_choice == 'камень':
-        stat['loses'] += 1
-        return f'я выйграл'
-    elif choice == 'ножницы' and computer_choice == 'бумага':
-        stat['wins'] += 1
-        return f'вы выйграли'
-    elif choice == 'бумага' and computer_choice == 'камень':
-        stat['wins'] += 1
-        return f'вы выйграли'
-    elif choice == 'бумага' and computer_choice == 'ножницы':
-        stat['loses'] += 1
-        return f'я выйграл'
-    else:
-        stat['ties'] += 1
-        return f'ничья'
-    
-stat = {
-    'games': 0,
-    'wins': 0,
-    'ties': 0,
-    'loses': 0,
-}
 
 
 @router.message(CommandStart())
